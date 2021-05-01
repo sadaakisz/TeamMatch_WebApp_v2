dc85cd931293d9c716e1771ff3690590a3263d93
Juan Carlos Hernández <jchernandez0312@gmail.com>
2021-05-01 18:37:37 -0500

    🎉Post with Axios
    🎉Connected values with DataBase

--- src/components/Profile/Profile.vue
+++ src/components/Profile/Profile.vue
@@ -4,18 +4,29 @@
       <b-nav-item
         ><p @click="route('/')"><img class="logo" src="./assets/logo.png" /></p
       ></b-nav-item>
-      <b-nav-item><p class="item" @click="route('/fillteam')">FILL MY TEAM</p></b-nav-item>
-      <b-nav-item><p class="item" @click="route('/findteam')">FIND A TEAM</p></b-nav-item>
+      <b-nav-item
+        ><p class="item" @click="route('/fillteam')">
+          FILL MY TEAM
+        </p></b-nav-item
+      >
+      <b-nav-item
+        ><p class="item" @click="route('/findteam')">FIND A TEAM</p></b-nav-item
+      >
       <b-nav-item><p class="item" @click="route('/')">CHAT</p></b-nav-item>
-      <b-nav-item><p class="item2" @click="route('/profile')">PROFILE</p></b-nav-item>
+      <b-nav-item
+        ><p class="item2" @click="route('/profile')">PROFILE</p></b-nav-item
+      >
       <b-dropdown
         text="Gino Quispe"
         class="dropd"
         right
         variant="outline-light"
-        
       >
-        <b-dropdown-item>Editar Perfil</b-dropdown-item>
+        <b-dropdown-item
+          id="show-btn"
+          @click="$bvModal.show('bv-modal-example')"
+          >Editar Perfil</b-dropdown-item
+        >
         <b-dropdown-item>Configuracion</b-dropdown-item>
         <b-dropdown-divider></b-dropdown-divider>
         <b-dropdown-item>Cerrar Sesion</b-dropdown-item>
@@ -31,27 +42,27 @@
         <b-row>
           <b-col cols="4">
             <img class="gino" src="./assets/gino.png" />
-            <h1>Gino Quispe</h1>
+            <h1>{{ fullName }}</h1>
             <h2>Premium</h2>
             <b-row class="my-3 espacio">
-              <b-col >Edad</b-col>
-              <b-col>19 años</b-col>
+              <b-col>Edad</b-col>
+              <b-col>{{ this.yearsOld }} años</b-col>
             </b-row>
             <b-row class="my-3 espacio">
-              <b-col >Horas Jugadas</b-col>
-              <b-col>320 h</b-col>
+              <b-col>Horas Jugadas</b-col>
+              <b-col>{{ this.hours }}</b-col>
             </b-row>
             <b-row class="my-3 espacio">
-              <b-col >K/D</b-col>
+              <b-col>K/D</b-col>
               <b-col>3.5</b-col>
             </b-row>
           </b-col>
 
           <b-col cols="8">
             <b-row class="my-5">
-              <b-col ><p class="subt">Game List</p></b-col>
-              <b-col ><p class="subt">My Rating</p></b-col>
-              <b-col ><p class="subt">Discord</p></b-col>
+              <b-col><p class="subt">Game List</p></b-col>
+              <b-col><p class="subt">My Rating</p></b-col>
+              <b-col><p class="subt">Discord</p></b-col>
             </b-row>
             <b-row class="my-5">
               <b-col><img src="./assets/game.png" /></b-col>
@@ -61,16 +72,145 @@
           </b-col>
         </b-row>
       </b-card>
+      <b-modal id="bv-modal-example" hide-footer>
+        <template #modal-title> Editar Perfil </template>
+        <div class="d-block text-center">
+          <h3>Hello {{this.fullName}}!</h3>
+        </div>
+        <form ref="form">
+        <b-form-group
+          label="Name"
+          label-for="name-input"
+          invalid-feedback="Name is required"
+        >
+          <b-form-input
+            id="name-input"
+            v-model="fullName"
+            required
+          ></b-form-input>
+        </b-form-group>
+        <b-form-group
+          label="Username"
+          label-for="username-input"
+          invalid-feedback="Username is required"
+        >
+          <b-form-input
+            id="username-input"
+            v-model="username"
+            required
+          ></b-form-input>
+        </b-form-group>
+                <b-form-group
+          label="description"
+          label-for="description-input"
+          invalid-feedback="description is required"
+        >
+          <b-form-input
+            id="description-input"
+            v-model="description"
+            required
+          ></b-form-input>
+        </b-form-group>
+                <b-form-group
+          label="Gender"
+          label-for="gender-input"
+          invalid-feedback="gender is required"
+        >
+          <b-form-input
+            id="gender-input"
+            v-model="gender"
+            required
+          ></b-form-input>
+        </b-form-group>
+                <b-form-group
+          label="Email"
+          label-for="email-input"
+          invalid-feedback="email is required"
+        >
+          <b-form-input
+            id="email-input"
+            v-model="email"
+            required
+          ></b-form-input>
+        </b-form-group>
+                <b-form-group
+          label="PhoneNumber"
+          label-for="phoneNumber-input"
+          invalid-feedback="phoneNumber is required"
+        >
+          <b-form-input
+            id="phoneNumber-input"
+            v-model="phoneNumber"
+            required
+          ></b-form-input>
+          </b-form-group>
+      </form>
+        <b-button class="mt-3" block @click="$bvModal.hide('bv-modal-example'); putData() "
+          >Guardar</b-button
+        >
+      </b-modal>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Profile",
+  created() {
+    this.getProfile();
+  },
+  data: () => ({
+    fullName: null,
+    hours: null,
+    date: null,
+    year: null,
+    yearsOld: null,
+    username: null,
+    description: null,
+    gender: null,
+    email: null,
+    phoneNumber: null,
+  }),
   methods: {
     route(location) {
       this.$router.push(location);
+    },
+
+    getProfile() {
+      axios
+        .get("https://teammatch-backend.herokuapp.com/api/players/1/profiles")
+        .then((response) => {
+          this.fullName = response.data.fullName;
+          this.date = response.data.birthDate;
+          this.year = new Date(this.date);
+          this.yearsOld = 2021 - parseInt(this.year.getFullYear());
+          this.username = response.data.username;
+          this.description = response.data.description;
+          this.gender = response.data.gender;
+          this.email = response.data.email;
+          this.phoneNumber = response.data.phoneNumber;
+        });
+      axios
+        .get("https://teammatch-backend.herokuapp.com/api/players/1")
+        .then((response) => {
+          this.hours = response.data.hours;
+        });
+    },
+    putData() {
+      axios.put(
+        "https://teammatch-backend.herokuapp.com/api/players/1/profiles",
+        {
+          fullName: this.fullName,
+          username: this.username,
+          description: this.description,
+          gender: this.gender,
+          email: this.email,
+          phoneNumber: this.phoneNumber,
+          birthDate: "2000-05-01T22:02:53.000+00:00",
+        }
+      );
     },
   },
 };
@@ -90,7 +230,7 @@ export default {
   justify-content: center;
 }
 
-.gino{
+.gino {
   height: 35%;
   margin-bottom: 1.5rem;
   margin-top: 1.5rem;
@@ -104,7 +244,7 @@ export default {
 
 .subt {
   font-size: 2rem;
-  border-bottom: 1px solid #FFF;
+  border-bottom: 1px solid #fff;
   display: inline;
 }
 
@@ -145,29 +285,27 @@ export default {
 }
 
 .item {
-  color: #FFF;
+  color: #fff;
   margin: 0;
   font-weight: 500;
-  font-size: .9rem;
-    &:hover {
-    color: #FF283D;
+  font-size: 0.9rem;
+  &:hover {
+    color: #ff283d;
     cursor: pointer;
-    border-bottom: 1px solid #FF283D;
+    border-bottom: 1px solid #ff283d;
   }
 }
 
 .item2 {
-  color: #FF283D;
+  color: #ff283d;
   margin: 0;
   font-weight: 500;
-  font-size: .9rem;
-  border-bottom: 1px solid #FF283D;
+  font-size: 0.9rem;
+  border-bottom: 1px solid #ff283d;
   display: inline;
-
-  
 }
 
-.dropd{
-  border:none;
+.dropd {
+  border: none;
 }
 </style>
