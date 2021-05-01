1c1ef574e6fb591ec532c98bbd3e2090ac577a22
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-05-01 18:28:02 -0500

    Navbar Template done.

--- src/components/RightTemplate.vue
+++ src/components/RightTemplate.vue
@@ -2,23 +2,45 @@
   <b-navbar class="navbar navbar-dark bg-transparent">
     <b-navbar-nav id="nav-text">
       <b-navbar-brand href="#">
-        <img src="../assets/TeamMatchLogo.png" class="d-inline-block align-top nav-logo" style="width: 41px; height: auto" alt="TeamMatch">
+        <img src="../assets/TeamMatchLogo.png" class="d-inline-block align-top nav-logo" style="width: 1.8vw; height: auto" alt="TeamMatch">
       </b-navbar-brand>
       <b-nav-item class="text-primary">
-        <span class="text active">AÑADE CLIENTES</span>
+        <span class="active">FILL MY TEAM</span>
       </b-nav-item>
-      <b-nav-item>VER CUENTAS</b-nav-item>
-      <b-nav-item>REGISTRAR PAGOS</b-nav-item>
-      <b-nav-item>REGISTRAR VENTAS</b-nav-item>
+      <b-nav-item>FIND A TEAM</b-nav-item>
+      <b-nav-item>CHAT</b-nav-item>
+      <b-nav-item>PROFILE</b-nav-item>
       <b-nav-item>
-        <b-button size="lg" class="nav-btn-registrarse">
-          <p class="text">REGISTRARSE</p>
-        </b-button>
+        <b-dropdown variant="link" toggle-class="text-decoration-none" class="nav-notification" no-caret right>
+          <template #button-content style="display: flex; justify-content: center; align-items: center">
+            <img src="../assets/NotificationBell.png" style="margin-top: -0.56vw; width: 1.77vw; height: auto">
+          </template>
+          <p class="not-title">Panel de<br>notificaciones</p>
+          <b-card bg-variant="Light" title="• Solicitud de team" class="not-card" style="border-radius: 20px; font-size: 1vw">
+            <b-card-text class="not-card-text">José Sanchez te ha invitado a unirse a su team.</b-card-text>
+          </b-card>
+        </b-dropdown>
       </b-nav-item>
       <b-nav-item>
-        <b-button size="lg" class="nav-btn-iniciar">
-          <p class="text">INICIAR SESIÓN</p>
-        </b-button>
+        <b-dropdown variant="link" toggle-class="text-decoration-none" class="nav-profile" right no-caret>
+          <template #button-content>
+            <div style="display: flex; align-items: center; justify-content: center; margin-top: -0.84vw">
+              <img src="../assets/ProfilePic.png" style="position:absolute; top: 0.32vw; left: 0.6vw; width: 2.86vw; height: auto">
+              <div style="margin-left: 2vw">
+                <p class="pr-name" style="position:absolute;top: 0.6vw; left: 4vw">Gino Quispe</p>
+                <p class="pr-plan" style="position:absolute;top: 2vw; left: 3.75vw">Premium</p>
+              </div>
+              <div><img src="../assets/ProfileDropdown.png" style="position:absolute; left: 10.5vw; top: 1.8vw"></div>
+            </div>
+          </template>
+          <p class="not-title pr-title">Acciones</p>
+          <b-button class="pr-action" href="/profile">
+            <div class="pr-text"><p>• Perfil</p></div>
+          </b-button>
+          <b-button class="pr-action" to="/">
+            <div class="pr-text"><p>• Cerrar sesión</p></div>
+          </b-button>
+        </b-dropdown>
       </b-nav-item>
     </b-navbar-nav>
   </b-navbar>
@@ -31,6 +53,7 @@ export default {
 </script>
 
 <style lang="scss">
+@import "../assets/scss/styles.scss";
 body {
   height: 100%;
   display: grid;
@@ -39,71 +62,5 @@ body {
   background-attachment: fixed;
   background-size: cover;
 }
-.text {
-  position: relative;
-  font-family: Gilroy ☞, Avenir, Helvetica, Arial, sans-serif;
-  font-size: 0.9em;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  font-weight: 700;
-  color: #fff;
-  margin: auto;
-}
-.navbar {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  top: 30px;
-  margin: 0 auto;
-  height: 3.6vw;
-  width: 100vw;
-}
 
-#nav-text > * {
-  margin-left: 2vw;
-  display: grid;
-  place-items: center;
-}
-.text-primary {
-  //background-image: url("../assets/ActiveTab.png");
-  background-size: 12vw auto;
-  background-repeat: no-repeat;
-  background-origin: border-box;
-  background-position: center 1.4em;
-  width: 12em;
-  .active {
-    color: #eee;
-    font-size: 1em;
-    position: relative;
-    left: 1em;
-  }
-}
-.nav-btn-registrarse {
-  transition: 0.3s;
-  width: 11.5vw;
-  height: 3.5vw;
-  border-radius: 4px;
-  background: linear-gradient(90deg, #4bc8f8 0%, #6e93f9 100%);
-}
-
-.nav-btn-registrarse:hover {
-  border: 3px solid #648dff;
-  background: transparent;
-}
-
-.nav-btn-iniciar {
-  transition: 0.3s;
-  width: 11.5vw;
-  height: 3.5vw;
-  border: 2px solid #fff;
-  border-radius: 4px;
-  background: transparent;
-}
-
-.nav-btn-iniciar:hover {
-  border-radius: 4px;
-  border: none;
-  background: linear-gradient(90deg, #4d56d1 0%, #da71ff 100%);
-}
 </style>
